7501b2bfb1950aa5739c8e9eecabdab293cbaf9b
Lokesh Bakawale <115867051+LokeshBakawale@users.noreply.github.com>
2022-12-06 23:28:06 +0530

    Prime Done

--- prime.js
+++ prime.js
@@ -1,15 +1,15 @@
-let num=9;
+let num=1;
 
 let count=0;
 
-for (let i=0;i<=num/2;i++){
+for (let i=0;i<=num;i++){
 
 	if(num%i==0){
 	count++;
 	}
 }
 
-if(count==0){
+if(count==2){
 console.log("Yes Prime");
 }
 
